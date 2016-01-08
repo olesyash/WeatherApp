747f508ad5be99c3d490c6fe7ff062c8a9913309
olesyash <ilana.olesya@gmail.com>
2016-01-08 18:56:07 +0200

    Added spinner with cities, and http request to weather server for city = "Jerusalem "

--- app/src/main/java/com/example/olesya/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/olesya/weatherapp/MainActivity.java
@@ -1,19 +1,41 @@
 package com.example.olesya.weatherapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Spinner citiesSpinner;
+    private static final String TAG = "MyDebug";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        citiesSpinner = (Spinner)findViewById(R.id.citiesSpinner);
+        citiesSpinner.setSelection(9);
+        citiesSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Log.i(TAG, "item selected");
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        httpRequest rq = new httpRequest(this);
+        rq.http_request("Jerusalem");
     }
 
-    @Override
+
+        @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
